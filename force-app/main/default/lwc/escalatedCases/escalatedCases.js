a68a7765caf3801623f9089ba2299426638a907e
ayushnvs <ayush.mishra.ald@gmail.com>
2023-09-26 10:47:25 +0530

    updated lwc

--- force-app/main/default/lwc/escalatedCases/escalatedCases.js
+++ force-app/main/default/lwc/escalatedCases/escalatedCases.js
@@ -4,4 +4,18 @@ import fromStatus from '@salesforce/apex/GetCasesClass.fromStatus';
 export default class EscalatedCases extends LightningElement {
     @wire(fromStatus, {status: 'Escalated'})
     cases;
+
+    get finalCases () {
+        this.cases.data.field.forEach(record => {
+            return record
+            // record.url = `https://tridevinc-dev-ed.develop.lightning.force.com/lightning/r/Case/${record.Id}/view`
+        });
+    }
+
+    // updateHref () {
+    //     this.a.href = this.li.querySelector('p').textContent
+    // }
 }
+
+// let test = new EscalatedCases()
+// test.updateHref();
