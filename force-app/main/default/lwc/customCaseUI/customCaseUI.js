055296210c222ea7f81a3a32617d08b6f96d020f
ayushnvs <ayush.mishra.ald@gmail.com>
2023-09-27 12:53:40 +0530

    added bootstrap and tested

--- force-app/main/default/lwc/customCaseUI/customCaseUI.js
+++ force-app/main/default/lwc/customCaseUI/customCaseUI.js
@@ -1,4 +1,7 @@
 import { LightningElement, api } from 'lwc';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import bootstrap from '@salesforce/resourceUrl/bootstrap';
 
 export default class CustomCaseUI extends LightningElement {
     @api recordId
@@ -6,6 +9,34 @@ export default class CustomCaseUI extends LightningElement {
     // caseRecordId = `Case: ${this.recordId}`;
 
     get caseRecordId () {
-        return 'Case: ' + this.recordId
+        return 'Case'
     }
+
+    // renderedCallback() {
+
+    //     Promise.all([
+    //         loadStyle(this, bootstrap + '/bootstrap-5.3.2-dist/css/bootstrap.css'),
+    //         loadScript(this, bootstrap + '/bootstrap-5.3.2-dist/js/bootstrap.js'),
+    //         // loadScript(this, popper),
+    //         // loadScript(this, jquery)     
+    //     ])
+    //     .then(() => {
+    //         this.dispatchEvent(
+    //             new ShowToastEvent({
+    //                 title: 'Success',
+    //                 message: 'Third-Party Libraries Loaded',
+    //                 variant: 'success',
+    //             }),
+    //         );
+    //     })
+    //     .catch(error => {
+    //         this.dispatchEvent(
+    //             new ShowToastEvent({
+    //                 title: 'Error Loading Third-Party Libraries',
+    //                 message: error.message,
+    //                 variant: 'error',
+    //             }),
+    //         );
+    //     });
+    // }
 }
