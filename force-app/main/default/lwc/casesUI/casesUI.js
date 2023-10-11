ed19f79c6587692ac3d01b22531a0dff64bd42d3
ayushnvs <ayush.mishra.ald@gmail.com>
2023-10-11 10:23:30 +0530

    upated

--- force-app/main/default/lwc/casesUI/casesUI.js
+++ force-app/main/default/lwc/casesUI/casesUI.js
@@ -1,6 +1,8 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getObjectInfo, getPicklistValuesByRecordType, createRecord } from 'lightning/uiObjectInfoApi';
 import CASE_OBJECT from "@salesforce/schema/Case";
+import doPost from '@salesforce/apex/GetCasesClass.createCase';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CasesUI extends LightningElement {
 
@@ -16,26 +18,26 @@ export default class CasesUI extends LightningElement {
 
     // @api recordId;
     // @api objectApiName; 
-    
+
     // objectName; 
 
     @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
-        accountMetadata;
+    accountMetadata;
 
-    @wire(getPicklistValuesByRecordType, { 
-            recordTypeId : '$accountMetadata.data.defaultRecordTypeId',
-            objectApiName : CASE_OBJECT
-        }) 
-        wiredRecordTypeInfo({data, error}) {
-            if(data) {
-                console.log(' getPicklistValuesByRecordType Info : ', data);
-                this.pickValStatus = data.picklistFieldValues.Status.values;
-                this.pickValOrigin = data.picklistFieldValues.Origin.values;
-                this.pickValPriority = data.picklistFieldValues.Priority.values;
-            }
-            if(error) {
-                console.log('Error Occurred : ', error);
-            }
+    @wire(getPicklistValuesByRecordType, {
+        recordTypeId: '$accountMetadata.data.defaultRecordTypeId',
+        objectApiName: CASE_OBJECT
+    })
+    wiredRecordTypeInfo({ data, error }) {
+        if (data) {
+            console.log(' getPicklistValuesByRecordType Info : ', data);
+            this.pickValStatus = data.picklistFieldValues.Status.values;
+            this.pickValOrigin = data.picklistFieldValues.Origin.values;
+            this.pickValPriority = data.picklistFieldValues.Priority.values;
+        }
+        if (error) {
+            console.log('Error Occurred : ', error);
+        }
     }
 
     handleChange(event) {
@@ -58,23 +60,43 @@ export default class CasesUI extends LightningElement {
 
     handleCreate() {
         console.log('Updating Salesforce Database!!')
-        // fields = {
-        //     'Case.subject': this.subject,
-        //     'Case.description': this.description,
-        //     'Case.status': this.statusValue,
-        //     'Case.origin': this.originValue,
-        //     'Case.priority': this.priorityValue
-        // }
+        fields = {
+            'subject': this.subject,
+            'description': this.description,
+            'status': this.statusValue,
+            'origin': this.originValue,
+            'priority': this.priorityValue
+        }
 
-        // const recordInput = {
-        //     apiName: CASE_OBJECT.objectApiName,
-        //     fields: fields
-        // }
+        const recordInput = {
+            apiName: CASE_OBJECT.objectApiName,
+            fields: fields
+        }
 
         console.log('Updating Salesforce Database!!')
         // createRecord(recordInput).then((record) => {
-        //     console.log(record);
-        //   });
+        //     console.log(record.Id);
+            // const toastEvent = new ShowToastEvent({
+            //     title:'Success!',
+            //     message:'Account created successfully',
+            //     variant:'success'
+            // });
+            // this.dispatchEvent(toastEvent);
+        // });
+
+        createCase(fields).then(record => {
+            console.log('Record Created', record.Id)
+            const toastEvent = new ShowToastEvent({
+                title:'Success!',
+                message:'Account created successfully',
+                variant:'success'
+            });
+            this.dispatchEvent(toastEvent);
+        }).catch(err => {
+            console.log(error);
+        })
     }
 
 }
+
+
